2237f8f1f7af6b7ce748c046b05a6aca8775afd9
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-11-11 18:24:58 +0000

    0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,66 +1,44 @@
 #!/usr/bin/python3
-"""
-    N-queen problem
-    The next algo solve any N queen in any NxN
-    Being N > 3
-"""
+""" N queens """
 import sys
 
 
-def n_q(t_arr, arr, col, i, n):
-    """
-       n_q - Find all posibles solution for N-queen problem and return it
-             in a list
-       @t_arr: temporaly list to store the all points of a posible solution
-       @arr: store all the solution
-       @col: save a colum use for a queen
-       @i: the row of the chess table
-       @n: Number of queens
-    """
-    if (i > n):
-        arr.append(t_arr[:])
-        return arr
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    for j in range(n + 1):
-        if i == 0 or ([i - 1, j - 1] not in t_arr and
-                      [i - 1, j + 1] not in t_arr and
-                      j not in col):
-            if i > 1:
-                dia = 0
-                for k in range(2, i + 1):
-                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
-                        dia = 1
-                        break
-                if dia:
-                    continue
-            t_arr.append([i, j])
-            col.append(j)
-            n_q(t_arr, arr, col, i + 1, n)
-            col.pop()
-            t_arr.pop()
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    return arr
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-    try:
-        n = int(sys.argv[1])
-    except BaseException:
-        print("N must be a number")
-        exit(1)
 
-    if not isinstance(n, int):
-        print("N must be a number")
-        exit(1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-    elif n < 4:
-        print("N must be at least 4")
-        exit(1)
 
-    n_q_arr = n_q([], [], [], 0, n - 1)
-    for i in n_q_arr:
-        print(i)
+solve(n)
